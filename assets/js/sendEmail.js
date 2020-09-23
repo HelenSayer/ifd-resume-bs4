bb5f153ef4e965647e482ed8df0b0defaa444eb0
Helen Sayer <helen.sayer10@gmail.com>
2020-09-23 22:18:28 +0000

    Implement emailjs integration on the contact page

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,9 @@
 function sendMail(contactForm) {
   emailjs
     .send("gmail", "rosie", {
-      from_name: contactForm.name.value,
-      from_email: contactForm.emailaddress.value,
-      project_request: contactForm.projectsummary.value,
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.emailaddress.value,
+      "project_request": contactForm.projectsummary.value,
     })
     .then(
       function (response) {
